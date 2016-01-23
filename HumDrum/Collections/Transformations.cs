bc7a2f955fb442bd35d72df335800acfbc8293d2
Nate Pisarski <nathanpisarski@gmail.com>
2016-01-23 16:47:21 -0500

    Add functions to Transformations

---
+++ HumDrum/Collections/Transformations.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumDrum.Collections
+{
+	public static class Transformations
+	{
+		/// <summary>
+		/// Gets the number of elements in this IEnumerable
+		/// </summary>
+		/// <param name="list">The list to count on</param>
+		/// <typeparam name="T">The type parameter</typeparam>
+		public static int Length<T>(this IEnumerable<T> list){
+			int counter = 0;
+
+			foreach (T item in list) 
+				counter++;
+			
+			return counter;			
+		}
+
+		/// <summary>
+		/// Get the item at the index from the list.
+		/// </summary>
+		/// <param name="list">The list</param>
+		/// <param name="index">The index (0-based)</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T Get<T>(this IEnumerable<T> list, int index){
+			int counter = 0;
+
+			foreach (T item in list) {
+				if (counter == index)
+					return item;
+				else
+					counter++;
+			}
+			throw new Exception ("Array Index out of bounds. Index: " + index + ". Array length: " + list.Length ());
+		}
+
+		/// <summary>
+		/// Use varargs to create an array literal
+		/// </summary>
+		/// <param name="items">The items to add to the array</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T[] Make<T>(params T[] items){
+			return items;
+		}
+
+		public static List<T> MakeList<T>(params T[] items){
+			return new List<T> (items);
+		}
+
+		/// <summary>
+		/// Create a subsequence of the given collection.
+		/// </summary>
+		/// <param name="list">The list to take the subsequence of</param>
+		/// <param name="start">Where to start collecting</param>
+		/// <param name="length">How many items to take</param>
+		/// <typeparam name="T">T - type</typeparam>
+		public static List<T> Subsequence<T>(IEnumerable<T> list, int start, int length){
+			var returnCollection = new List<T> ();
+
+			for (int i = start; i < list.Length (); i++)
+				returnCollection.Add (list.Get (i));
+
+			return returnCollection;
+		}
+
+		/// <summary>
+		/// Collects elements of the list while the predicate is true, and includes
+		/// the element that causes failure.
+		/// </summary>
+		/// <returns>The list</returns>
+		/// <param name="list">The data to filter</param>
+		/// <param name="predicate">The predicate used for filtering</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static List<T> WhileInclusive<T>(IEnumerable<T> list, Predicate<T> predicate)
+		{
+			var collected = new List<T> ();
+
+			foreach (T item in list) {
+				if (!predicate(item)) {
+					collected.Add (item);
+					break;
+				}
+
+				collected.Add (item);
+			}
+
+			return collected;
+		}
+
+		/// <summary>
+		/// Collects the elements of the list after an element causes the predicate to be true.
+		/// This includes the element which causes it.
+		/// </summary>
+		/// <returns>The trigger, and everything after it/returns>
+		/// <param name="list">The list to filter</param>
+		/// <param name="predicate">The predicate used for filtering</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static List<T> AfterInclusive<T>(IEnumerable<T> list, Predicate<T> predicate)
+		{
+			var collected = new List<T> ();
+			bool collecting = false;
+
+			foreach (T item in list) {
+				if (predicate (item)) {
+					collected.Add (item);
+					collecting = true;
+					continue;
+				} else if (collecting)
+					collected.Add (item);
+			}
+
+			return collected;
+		}
+
+		/// <summary>
+		/// Collects the elements of a list until the predicate is false.
+		/// </summary>
+		/// <param name="list">The list to filter</param>
+		/// <param name="predicate">Predicate.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static List<T> While<T>(IEnumerable<T> list, Predicate<T> predicate)
+		{
+			var temp = WhileInclusive (list, predicate);
+			temp.RemoveAt (temp.Count - 1);
+
+			return temp;
+		}
+
+
+		public static List<T> After<T>(IEnumerable<T> list, Predicate<T> predicate)
+		{
+			var temp = AfterInclusive (list, predicate);
+			temp.RemoveAt (0);
+			return temp;
+		}
+
+	}
+}
+
---
+++ HumDrum/Structures/Tree.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HumDrum.Recursion;
+
+namespace HumDrum.Structures
+{
+	public class Tree<T>
+	{
+		/// <summary>
+		/// This current node
+		/// </summary>
+		public T CurrentNode { get; set; }
+
+		/// <summary>
+		/// The left branch
+		/// </summary>
+		/// <value>The left branch.</value>
+		public Tree<T> LeftBranch { get; set; }
+
+		/// <summary>
+		/// The right branch of this binary tree
+		/// </summary>
+		/// <value>The right branch.</value>
+		public Tree<T> RightBranch {get; set;}
+
+		public Tree<T> Parent { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of a tree
+		/// </summary>
+		public Tree (T currentValue, Tree<T> parent)
+		{
+			CurrentNode = currentValue;
+
+			LeftBranch = null;
+			RightBranch = null;
+
+			Parent = parent;
+		}
+
+		/// <summary>
+		/// Get the value from the branch in the given direction
+		/// </summary>
+		/// <returns>The value</returns>
+		/// <param name="direction">LEFT or RIGHT</param>
+		public T GetValue(Direction direction){
+			switch (direction)
+			{
+			case Direction.LEFT:
+				return LeftBranch.CurrentNode;
+			case Direction.RIGHT:
+				return RightBranch.CurrentNode;
+			case Direction.UP:
+				return Parent.CurrentNode;
+			}
+
+			return default(T);
+		}
+
+		/// <summary>
+		/// Grow the tree in the direction with a seed value
+		/// </summary>
+		/// <param name="value">The value to use</param>
+		/// <param name="direction">Which direction to grow</param>
+		public void Grow(T value, Direction direction){
+			if (direction == Direction.RIGHT)
+				RightBranch = new Tree<T> (value, this);
+			else if (direction == Direction.LEFT)
+				LeftBranch = new Tree<T> (value, this);
+		}
+
+		/// <summary>
+		/// Flattens the entire tree into a list of its Nodes. Working on these directly
+		/// affects the information in the tree, so be careful.
+		/// </summary>
+		public List<T> Flatten(){
+			if (RightBranch == null && LeftBranch == null)
+				return TailHelper.Wrap (CurrentNode);
+			else
+				return TailHelper.Concatenate( // Returns the current node and all the other nodes
+					TailHelper.Wrap(CurrentNode),
+					TailHelper.Concatenate (RightBranch.Flatten (), LeftBranch.Flatten ()));
+		}
+
+		/// <summary>
+		/// Destroy all sub-trees from this node, not including this one.
+		/// </summary>
+		public void Snip(){
+			LeftBranch  = null;
+			RightBranch = null;
+		}
+
+		/// <summary>
+		/// Map the specified function over this tree
+		/// </summary>
+		/// <param name="function">The function to map</param>
+		public void Map(Action<T> function){
+			Flatten ().ForEach (function);
+		}
+
+		/// <summary>
+		/// "Prune" branches of this tree after the predicate returns false.
+		/// </summary>
+		/// <param name="pred">Pred.</param>
+		public void Prune(Predicate<T> pred){
+			if (!pred (CurrentNode))
+				Parent.Snip ();
+			else {
+				LeftBranch.Prune (pred);
+				RightBranch.Prune (pred);
+			}
+		}
+	}
+}
+
